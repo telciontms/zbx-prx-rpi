62238e147292e3c447a4f686a93bdfa641d6e9c2
Blake Carpenter <blakecarpenter@Blakes-MacBook-Pro.local>
2020-03-11 00:27:18 -0700

    Updated script based on rpi_setup.sh script changes.

--- rpi_zabbix_config.sh
+++ rpi_zabbix_config.sh
@@ -3,6 +3,7 @@
 #Zabbix Configuration script
 # Created by Blake Carpenter for Telcion Communications Group
 #
+# Configuring Zabbix application
 # Checking for existence of zabbix_proxy.conf backup file
 if [ -f "/etc/zabbix/zabbix_proxy.conf.orig" ]; then
 	echo "zabbix_proxy.conf.orig exists"
@@ -13,30 +14,57 @@ else
 	cp /etc/zabbix/zabbix_proxy.conf /etc/zabbix/zabbix_proxy.conf.orig
 fi
 #
+#
+#
 # Specify Zabbix Proxy Hostname
 echo "Please specify the Zabbix Proxy Hostname."
 echo "Format of the Zabbix Proxy Hostname should be ###-ClientName"
 read -p 'Zabbix Proxy Hostame: ' HOSTNAMEVAR
-echo "You have chosen $(tput setaf 1)$HOSTNAMEVAR $(tput sgr 0)."
-echo "Please be sure to use $(tput setaf 1)$HOSTNAMEVAR $(tput sgr 0)as the proxy name when you name your proxy server in Zabbix."
+#
 #
 #
 # Configuring Zabbix Server
-echo "Setting tms.telcion.com as the destination server..."
+echo "Please specify the Zabbix Server"
+read -p 'Zabbix Server: ' ZABBIXSERVER
+echo "Setting $ZABBIXSERVER as the Zabbix Server..."
 sleep 5
-sed -i 's/Server=127.0.0.1/Server=tms.telcion.com/g' /etc/zabbix/zabbix_proxy.conf
+sed -i "s/Server=127.0.0.1/Server=$ZABBIXSERVER/g" /etc/zabbix/zabbix_proxy.conf
 echo "Setting $HOSTNAMEVAR as the proxy name..."
 sleep 5
 sed -i "s/Hostname=Zabbix proxy/Hostname=$HOSTNAMEVAR/g" /etc/zabbix/zabbix_proxy.conf
 #
 #
+#
+# Configuring Zabbix Proxy Parameters
+echo Configuring Zabbix Proxy DB Connection Parameters...
+sed -i 's/DBName=zabbix_proxy/DBName=zabbix/g' /etc/zabbix/zabbix_proxy.conf
+sed -i "s/# DBPassword=/DBPassword=$MYSQLZABBIXPW/g" /etc/zabbix/zabbix_proxy.conf
+sleep 5
+echo Configuring zabbix proxy configuration retrieval frequency
+sed -i 's/# ConfigFrequency=3600/ConfigFrequency=1200/g' /etc/zabbix/zabbix_proxy.conf
+sleep 5
+echo Configuring other zabbix proxy parameters...
+sed -i 's/# StartPollers=5/StartPollers=10/g' /etc/zabbix/zabbix_proxy.conf
+sed -i 's/# StartPollerUnreachable=1/StartPollerUnreachable=5/g' /etc/zabbix/zabbix_proxy.conf
+sed -i 's/# StartPingers=1/StartPingers=10/g' /etc/zabbix/zabbix_proxy.conf
+sed -i 's/# StartDiscoverers=1/StartDiscoverers=5/g' /etc/zabbix/zabbix_proxy.conf
+sed -i 's/# StartVMwareCollectors=0/StartVMwareCollectors=5/g' /etc/zabbix/zabbix_proxy.conf
+sed -i 's/# VMwareCacheSize=8M/VMwareCacheSize=100M/g' /etc/zabbix/zabbix_proxy.conf
+sed -i 's/# CacheSize=8M/CacheSize=100M/g' /etc/zabbix/zabbix_proxy.conf
+sed -i 's/# AllowRoot=0/AllowRoot=1/g' /etc/zabbix/zabbix_proxy.conf
+sleep 10
+echo Zabbix Proxy Parameters have been configured
+#
+#
+#
 # Configuring TLS PSK Encryption
 echo Configuring TLS settings...
 sleep 5
-echo "Please specify the Zabbix Proxy PSK ID.  This should be in the format of PSK###.  You should use the same number as you specified in the hostname."
-echo "If you specified the name to be 999-ClientName then your PSK ID should be PSK999.  Make note of this PSK ID when you add your proxy to the Zabbix Server."
+echo "Please specify the Zabbix Proxy PSK ID.  This should be in the format of PSK###."
+echo "You should use the same number as you specified in the hostname ($HOSTNAMEVAR)."
 read -p 'Zabbix Proxy PSK ID: ' PSKID
-echo "You have chosen $(tput setaf 1)$PSKID $(tput sgr 0)."
+echo "You have chosen $PSKID."
+#
 #
 #
 # Configuring PSK ID
@@ -47,18 +75,33 @@ sed -i "s/# TLSPSKIdentity=/TLSPSKIdentity=$PSKID/g" /etc/zabbix/zabbix_proxy.co
 sed -i "s+# TLSPSKFile=+TLSPSKFile=/etc/zabbix/zabbix_proxy.psk+g" /etc/zabbix/zabbix_proxy.conf
 #
 #
+#
 # Configuring PSK Encryption Key
 echo "Creating PSK Encryption Key..."
 sleep 5
 touch /etc/zabbix/zabbix_proxy.psk
 openssl rand -hex 32 > /etc/zabbix/zabbix_proxy.psk
-echo "Please make note of the following encryption key as you will need it when you add this proxy to the Zabbix Server:"
-echo $(tput setaf 1)$(cat /etc/zabbix/zabbix_proxy.psk) $(tput sgr 0)
+#
 #
 #
 # Gather WAN IP Address Information
 echo "Gathering WAN IP Address Information..."
 sleep 5
-echo "Your WAN IP address is:"
-echo $(tput setaf 1) $(curl -s ipv4.icanhazip.com) $(tput sgr 0)
-echo "Please use the above information in order to create the proxy connection in the Zabbix server.
+#
+#
+#
+# Instruct to add information to Zabbix Server
+echo "Please add the following information to the Zabbix Server for the Zabbix Proxy setup:"
+echo "Zabbix Proxy Name: $(tput setaf 1)$HOSTNAMEVAR $(tput sgr 0)"
+echo "Zabbix Proxy PSKID: $(tput setaf 1)$PSKID $(tput sgr 0)"
+echo "Zabbix Proxy Encryption Key: $(tput setaf 1)$(cat /etc/zabbix/zabbix_proxy.psk) $(tput sgr 0)"
+echo "Zabbix Proxy WAN IP Address: echo $(tput setaf 1) $(curl -s ipv4.icanhazip.com) $(tput sgr 0)"
+echo " "
+echo " "
+read -p "Once the above information has been added to the Zabbix Server, press [Enter] to restart the zabbix proxy..." NULL
+#
+#
+#
+# Configuring Zabbix Proxy Service
+echo Restarting Zabbix Proxy Service...
+systemctl restart zabbix-proxy
